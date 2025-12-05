94f70264a9069e3758b0b218f626acc6013e9e68
jonsetzky <40266374+jonsetzky@users.noreply.github.com>
2025-12-05 15:44:29 +0200

    complete 01 of 2025

--- year_2025/src/bin/01/main.rs
+++ year_2025/src/bin/01/main.rs
@@ -1,3 +1,32 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("HELO");
+    let input_string = read_to_string("src/bin/01/input.txt").expect("Failed to read file");
+
+    let mut dial = 50;
+    let mut zero_count = 0;
+
+    for line in input_string.lines() {
+        let mut chars = line.chars();
+        let dir = chars.next().unwrap();
+        let value: i32 = chars.collect::<String>().parse().unwrap();
+
+        match dir {
+            'L' => dial -= value,
+            'R' => dial += value,
+            _ => (),
+        }
+
+        if dial < 0 {
+            dial += 100 * ((-(dial as f64) / 100.0).floor() as i32 + 1);
+        }
+
+        dial %= 100;
+
+        if dial == 0 {
+            zero_count += 1;
+        }
+    }
+    println!("Final dial position: {}", dial);
+    println!("Number of times dial was at 0: {}", zero_count);
 }
