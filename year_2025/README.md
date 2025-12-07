3bbc7fa63608b3892a303bf99da88a79d7e299e8
jonsetzky <40266374+jonsetzky@users.noreply.github.com>
2025-12-07 18:50:57 +0200

    complete day 4 part 1

--- year_2025/README.md
+++ year_2025/README.md
@@ -9,3 +9,4 @@
 |     | 2     | 135.7439ms      | this is how i initially implemented it, so it was really quick to complete |
 | 3   | 1     | 228.1µs         |                                                                            |
 |     | 2     | 934.7µs         | completely different approach than part 1                                  |
+| 4   | 1     | 20.5275ms       | original solution took 9 seconds but switching to hashmap fixed it         |
--- year_2025/src/bin/04/main.rs
+++ year_2025/src/bin/04/main.rs
@@ -1,7 +1,74 @@
-use std::{cmp::min, fs::read_to_string};
+use std::{collections::HashMap, fs::read_to_string};
+
+const MAX_ADJACENT: isize = 4;
 
 fn count_accessable_rolls(grid: &str) -> usize {
-    12
+    let w = grid.lines().next().unwrap().len();
+    let h = grid.lines().count();
+    let grid_no_newlines = grid.replace("\n", "");
+
+    let total_rolls = grid_no_newlines.chars().filter(|c| *c == '@').count();
+    let mut rolls: HashMap<(usize, usize), isize> = HashMap::new();
+
+    println!("Grid size: {} x {}, {} rolls", w, h, total_rolls);
+
+    let coords = |i: usize| -> (usize, usize) { (i % w, i / w) };
+
+    grid_no_newlines
+        .char_indices()
+        .filter(|(_, c)| *c == '@')
+        .for_each(|(i, _)| {
+            let (x, y) = coords(i);
+            let mut count = 8isize - MAX_ADJACENT;
+            if x == 0 || x == w - 1 {
+                count -= 3;
+                if y == 0 || y == h - 1 {
+                    count -= 2;
+                }
+            } else if y == 0 || y == h - 1 {
+                count -= 3;
+            }
+            rolls.insert((x, y), count);
+        });
+
+    grid_no_newlines
+        .char_indices()
+        .filter(|(_, c)| *c == '.')
+        .for_each(|(i, _)| {
+            let (x, y) = coords(i);
+
+            (-1..=1).for_each(|dy| {
+                (-1..=1).for_each(|dx| {
+                    if dx == 0 && dy == 0 {
+                        return;
+                    }
+                    let nx = x as isize + dx;
+                    let ny = y as isize + dy;
+                    if nx < 0 || nx >= w as isize || ny < 0 || ny >= h as isize {
+                        return;
+                    }
+                    let roll = rolls.get(&(nx as usize, ny as usize));
+
+                    if roll.is_some() {
+                        rolls.insert((nx as usize, ny as usize), roll.unwrap() - 1);
+                    }
+                });
+            });
+        });
+
+    // for j in 0..h {
+    //     for i in 0..w {
+    //         print!(
+    //             "{}",
+    //             rolls
+    //                 .get(&(i, j))
+    //                 .map(|x| x.to_string().chars().take(1).next().unwrap())
+    //                 .unwrap_or('.')
+    //         );
+    //     }
+    //     println!()
+    // }
+    rolls.into_iter().filter(|(_, count)| *count < 0).count() as usize
 }
 
 fn main() {
@@ -9,34 +76,33 @@ fn main() {
 
     let start = std::time::Instant::now();
 
-    // todo
+    let count = count_accessable_rolls(&input_string);
 
     let duration = start.elapsed();
 
-    println!("Result todo");
+    println!("Result: {}", count);
     println!("Time elapsed: {:?}", duration);
 }
 
 #[cfg(test)]
-mod tests {
+mod day04_tests {
     use super::*;
     use test_case::test_case;
 
     #[test_case(
-        "..xx.xx@x.
-x@@.@.@.@@
-@@@@@.x.@@
+        r##"..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
 @.@@@@..@.
-x@.@@@@.@x
+@@.@@@@.@@
 .@@@@@@@.@
 .@.@.@.@@@
-x.@@@.@@@@
+@.@@@.@@@@
 .@@@@@@@@.
-x.x.@@@.x.
-",
-        13
+@.@.@@@.@."##,
+        13_usize
     )]
-    fn it_works(grid: &str, expected: usize) {
+    fn it_works(grid: &'static str, expected: usize) {
         assert_eq!(expected, count_accessable_rolls(grid));
     }
 }
